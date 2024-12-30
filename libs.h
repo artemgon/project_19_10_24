79ef210a4a6d817c47da62b3580b0ec1ee98159c
artemgon <artemhoncharuk123@gmail.com>
2024-12-30 23:48:01 +0200

    Developer branch is created.

---
+++ libs.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <conio.h>
+#include <windows.h>
+#include <cmath>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdarg>
+#include <cctype>
+#include <cstring> 
+#include <stack>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
